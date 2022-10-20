a6514a43911de57d23ffb87517d9a35291cdc434
davidangeles <lolo.2210.lolo@gmail.com>
2022-10-20 15:42:44 -0500

    Rename module

--- ms-persistence/src/main/java/com/nttdata/bootcamp/mspersistence/MsPersistenceApplication.java
+++ ms-persistence/src/main/java/com/nttdata/bootcamp/mspersistence/MsPersistenceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsDatabaseApplication {
+public class MsPersistenceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsDatabaseApplication.class, args);
+        SpringApplication.run(MsPersistenceApplication.class, args);
     }
 }
